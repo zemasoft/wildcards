074e6001368cf3c3f8844d82df939c4fac85b4b3
Tomas Zeman <zemasoft@seznam.cz>
2018-04-04 08:18:06 +0200

    Recursive (C++11) implementation of equal

--- include/cx/algorithm.hpp
+++ include/cx/algorithm.hpp
@@ -12,12 +12,9 @@ namespace cx
 template <typename Iterator1, typename Iterator2>
 constexpr bool equal(Iterator1 first1, Iterator1 last1, Iterator2 first2, Iterator2 last2)
 {
-  while (first1 != last1 && first2 != last2 && *first1 == *first2)
-  {
-    ++first1, ++first2;
-  }
-
-  return first1 == last1 && first2 == last2;
+  return first1 != last1 && first2 != last2 && *first1 == *first2
+             ? equal(first1 + 1, last1, first2 + 1, last2)
+             : first1 == last1 && first2 == last2;
 }
 
 }  // namespace cx
