14cfea507822061b98b78cc02de0430b17e48fc3
Tomas Zeman <zemasoft@seznam.cz>
2018-04-12 11:50:12 +0200

    Extend test

--- test/src/wildcards/wildcards_test.cpp
+++ test/src/wildcards/wildcards_test.cpp
@@ -3,21 +3,87 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#include <cx/string_view.hpp>
-#include <wildcards/wildcards.hpp>
+#include <cx/string_view.hpp>       // cx::literals
+#include <wildcards/wildcards.hpp>  // wildcards::match
 
 #include <catch.hpp>
 
-TEST_CASE("wildcards", "wildcards")
+TEST_CASE("wildcards::match() is compliant", "[wildcards::match]")
 {
   using namespace cx::literals;
 
-  static_assert(wildcards::match("test"_sv, "test"_sv), "");
-  static_assert(wildcards::match("test"_sv, "t*st"_sv), "");
-  static_assert(wildcards::match("test"_sv, "t?st"_sv), "");
-  static_assert(wildcards::match("test"_sv, "t*t"_sv), "");
+  SECTION("wildcards::match(cx::string_view, cx::string_view)")
+  {
+    SECTION("match with an empty pattern")
+    {
+      constexpr auto pattern = ""_sv;
 
-  constexpr auto cards = wildcards::make_cards(u'*', u'.');
-  static_assert(wildcards::match(u"Ahoj, jak se mas?"_sv, u"Ahoj, *m..?"_sv, cards), "");
-  static_assert(wildcards::match(u"Ahoj, jak se mas?"_sv, u"Ahoj, *m..?"_sv, {u'*', u'.'}), "");
+      static_assert(wildcards::match(""_sv, pattern), "");
+
+      static_assert(!wildcards::match("Anything"_sv, pattern), "");
+    }
+
+    SECTION("match with \"A\"")
+    {
+      constexpr auto pattern = "A"_sv;
+
+      static_assert(wildcards::match("A"_sv, pattern), "");
+
+      static_assert(!wildcards::match(""_sv, pattern), "");
+      static_assert(!wildcards::match("a"_sv, pattern), "");
+      static_assert(!wildcards::match("AA"_sv, pattern), "");
+      static_assert(!wildcards::match("Something"_sv, pattern), "");
+    }
+
+    SECTION("match with \"Hallo!\"")
+    {
+      constexpr auto pattern = "Hallo!"_sv;
+
+      static_assert(wildcards::match("Hallo!"_sv, pattern), "");
+
+      static_assert(!wildcards::match(""_sv, pattern), "");
+      static_assert(!wildcards::match("Hello!"_sv, pattern), "");
+      static_assert(!wildcards::match("HHallo!"_sv, pattern), "");
+      static_assert(!wildcards::match("Hallo!!"_sv, pattern), "");
+      static_assert(!wildcards::match("Hallo!Hallo!"_sv, pattern), "");
+    }
+
+    SECTION("match with \"*\"")
+    {
+      constexpr auto pattern = "*"_sv;
+
+      static_assert(wildcards::match(""_sv, pattern), "");
+      static_assert(wildcards::match("Anything"_sv, pattern), "");
+    }
+
+    SECTION("match with \"?\"")
+    {
+      constexpr auto pattern = "?"_sv;
+
+      static_assert(wildcards::match("A"_sv, pattern), "");
+      static_assert(wildcards::match("a"_sv, pattern), "");
+
+      static_assert(!wildcards::match(""_sv, pattern), "");
+      static_assert(!wildcards::match("Something"_sv, pattern), "");
+    }
+
+    SECTION("match with \"H?llo,*W*!\"")
+    {
+      constexpr auto pattern = "H?llo,*W*!"_sv;
+
+      static_assert(wildcards::match("Hallo, World!"_sv, pattern), "");
+      static_assert(wildcards::match("Hello, World!"_sv, pattern), "");
+      static_assert(wildcards::match("Hello,World!"_sv, pattern), "");
+      static_assert(wildcards::match("Hello,WildCards!"_sv, pattern), "");
+      static_assert(wildcards::match("Hello, crazy WildCards!"_sv, pattern), "");
+      static_assert(wildcards::match("Hello, crazy WildCards! Still working?!"_sv, pattern), "");
+
+      static_assert(!wildcards::match(""_sv, pattern), "");
+      static_assert(!wildcards::match("Hllo, World!"_sv, pattern), "");
+      static_assert(!wildcards::match("Hallo, World?"_sv, pattern), "");
+      static_assert(!wildcards::match("Hallo, world!"_sv, pattern), "");
+      static_assert(!wildcards::match("Yes. Hallo, World!"_sv, pattern), "");
+      static_assert(!wildcards::match("Hallo, World!?"_sv, pattern), "");
+    }
+  }
 }
