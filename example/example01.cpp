e460c7f2c823051407b69814726ad549707b3a5d
Tomas Zeman <zemasoft@seznam.cz>
2018-04-17 09:44:19 +0200

    Extend example 1

--- example/example01.cpp
+++ example/example01.cpp
@@ -1,11 +1,22 @@
-#include "cx/string_view.hpp"
-#include "wildcards/wildcards.hpp"
+// Copyright Tomas Zeman 2018.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include "wildcards/wildcards.hpp"  // wildcards::match
 
 int main()
 {
-  using namespace cx::literals;
+  constexpr char pattern[] = "Hallo,* World?";
 
-  static_assert(wildcards::match("Hallo"_sv, "Hallo"_sv), "");
+  static_assert(wildcards::match("Hallo, World!", pattern), "");
+  static_assert(wildcards::match("Hallo, World?", pattern), "");
+  static_assert(wildcards::match("Hallo, dear World!", pattern), "");
+
+  static_assert(!wildcards::match("Hallo World!", pattern), "");
+  static_assert(!wildcards::match("Hallo,World!", pattern), "");
+  static_assert(!wildcards::match("Hallo, world!", pattern), "");
+  static_assert(!wildcards::match("Hallo, World", pattern), "");
 
   return 0;
 }
