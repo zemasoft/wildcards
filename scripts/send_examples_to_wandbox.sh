2ace858c07443a76ea4ca3226e49903d4abdb311
Tomas Zeman <zemasoft@seznam.cz>
2018-04-17 19:52:57 +0200

    Rename script

--- scripts/send_examples_to_wandbox.sh
+++ scripts/send_examples_to_wandbox.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Copyright Tomas Zeman 2018.
-# Distributed under the Boost Software License, Version 1.0.
-# (See accompanying file LICENSE_1_0.txt or copy at
-# http://www.boost.org/LICENSE_1_0.txt)
-
 readonly WANDBOX_DIR="wandbox"
 
 rm -rf "$WANDBOX_DIR"
