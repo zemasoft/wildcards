844d22c781b98170a928ed391e15867f82c25df1
Tomas Zeman <zemasoft@seznam.cz>
2018-04-19 14:16:50 +0200

    Restructure examples. Add example

--- example/example02.cpp
+++ example/example02.cpp
@@ -11,116 +11,51 @@
 #include "wildcards/match.hpp"
 
 template <typename T>
-struct valid_id_pattern;
-
-template <>
-struct valid_id_pattern<char>
+constexpr cx::basic_string_view<T> basic_valid_id(cx::basic_string_view<T> s,
+                                                  cx::basic_string_view<T> p)
 {
-  constexpr static cx::string_view value()
-  {
-    return "test_*";
-  }
-};
-
-template <>
-struct valid_id_pattern<char16_t>
-{
-  constexpr static cx::u16string_view value()
-  {
-    return u"test_*";
-  }
-};
-
-template <>
-struct valid_id_pattern<char32_t>
-{
-  constexpr static cx::u32string_view value()
-  {
-    return U"test_*";
-  }
-};
-
-template <>
-struct valid_id_pattern<wchar_t>
-{
-  constexpr static cx::wstring_view value()
-  {
-    return L"test_*";
-  }
-};
-
-template <typename T>
-constexpr cx::basic_string_view<T> basic_valid_id(cx::basic_string_view<T> s)
-{
-  return wildcards::match(s, valid_id_pattern<T>::value()) ? s
-                                                           : throw std::logic_error("Invalid ID");
+  return wildcards::match(s, p) ? s : throw std::logic_error("Invalid ID");
 }
 
-constexpr cx::string_view valid_id(cx::string_view s)
+constexpr cx::string_view valid_id(cx::string_view s, cx::string_view p)
 {
-  return basic_valid_id<char>(s);
+  return basic_valid_id<char>(s, p);
 }
 
-constexpr cx::u16string_view valid_id(cx::u16string_view s)
+constexpr cx::u16string_view valid_id(cx::u16string_view s, cx::u16string_view p)
 {
-  return basic_valid_id<char16_t>(s);
+  return basic_valid_id<char16_t>(s, p);
 }
 
-constexpr cx::u32string_view valid_id(cx::u32string_view s)
+constexpr cx::u32string_view valid_id(cx::u32string_view s, cx::u32string_view p)
 {
-  return basic_valid_id<char32_t>(s);
+  return basic_valid_id<char32_t>(s, p);
 }
 
-constexpr cx::wstring_view valid_id(cx::wstring_view s)
+constexpr cx::wstring_view valid_id(cx::wstring_view s, cx::wstring_view p)
 {
-  return basic_valid_id<wchar_t>(s);
-}
-
-constexpr cx::string_view operator"" _valid_id(const char* str, std::size_t s)
-{
-  return valid_id(cx::make_string_view(str, s));
-}
-
-constexpr cx::u16string_view operator"" _valid_id(const char16_t* str, std::size_t s)
-{
-  return valid_id(cx::make_string_view(str, s));
-}
-
-constexpr cx::u32string_view operator"" _valid_id(const char32_t* str, std::size_t s)
-{
-  return valid_id(cx::make_string_view(str, s));
-}
-
-constexpr cx::wstring_view operator"" _valid_id(const wchar_t* str, std::size_t s)
-{
-  return valid_id(cx::make_string_view(str, s));
+  return basic_valid_id<wchar_t>(s, p);
 }
 
 int main()
 {
   // compile time
   {
-    constexpr auto id1 = valid_id("test_something");
+    constexpr auto id1 = valid_id("test_something", "test_*");
     std::cout << "Valid ID: " << id1 << std::endl;
 
-    constexpr auto id2 = "test_something_else"_valid_id;
-    std::cout << "Valid ID: " << id2 << std::endl;
-
-    // constexpr auto id3 = "tst_something_different"_valid_id;  // compile time error
-    // std::cout << "Valid ID: " << id3 << std::endl;
+    // constexpr auto id2 = valid_id("test_something_else", "Test_*");  // compile time error
+    // std::cout << "Valid ID: " << id2 << std::endl;
   }
 
   // runtime
   try
   {
-    auto id1 = valid_id("test_something");
+    auto id1 = valid_id("test_something", "test_*");
     std::cout << "Valid ID: " << id1 << std::endl;
 
-    auto id2 = "test_something_else"_valid_id;
+    auto id2 = valid_id("test_something_else", "Test_*");  // runtime error
     std::cout << "Valid ID: " << id2 << std::endl;
-
-    auto id3 = "tst_something_different"_valid_id;  // runtime error
-    std::cout << "Valid ID: " << id3 << std::endl;
   }
   catch (const std::exception& e)
   {
--- example/example03.cpp
+++ example/example03.cpp
@@ -3,32 +3,128 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#include <cstring>
+#include <exception>
 #include <iostream>
+#include <stdexcept>
 
 #include "cx/string_view.hpp"
 #include "wildcards/match.hpp"
 
-int main(int argc, char** argv)
+template <typename T>
+struct valid_id_pattern;
+
+template <>
+struct valid_id_pattern<char>
 {
-  if (argc <= 1)
+  constexpr static cx::string_view value()
   {
-    std::cout << "usage: " << argv[0] << " string pattern" << std::endl;
-    return EXIT_SUCCESS;
+    return "test_*";
+  }
+};
+
+template <>
+struct valid_id_pattern<char16_t>
+{
+  constexpr static cx::u16string_view value()
+  {
+    return u"test_*";
+  }
+};
+
+template <>
+struct valid_id_pattern<char32_t>
+{
+  constexpr static cx::u32string_view value()
+  {
+    return U"test_*";
+  }
+};
+
+template <>
+struct valid_id_pattern<wchar_t>
+{
+  constexpr static cx::wstring_view value()
+  {
+    return L"test_*";
+  }
+};
+
+template <typename T>
+constexpr cx::basic_string_view<T> basic_valid_id(cx::basic_string_view<T> s)
+{
+  return wildcards::match(s, valid_id_pattern<T>::value()) ? s
+                                                           : throw std::logic_error("Invalid ID");
+}
+
+constexpr cx::string_view valid_id(cx::string_view s)
+{
+  return basic_valid_id<char>(s);
+}
+
+constexpr cx::u16string_view valid_id(cx::u16string_view s)
+{
+  return basic_valid_id<char16_t>(s);
+}
+
+constexpr cx::u32string_view valid_id(cx::u32string_view s)
+{
+  return basic_valid_id<char32_t>(s);
+}
+
+constexpr cx::wstring_view valid_id(cx::wstring_view s)
+{
+  return basic_valid_id<wchar_t>(s);
+}
+
+constexpr cx::string_view operator"" _valid_id(const char* str, std::size_t s)
+{
+  return valid_id(cx::make_string_view(str, s));
+}
+
+constexpr cx::u16string_view operator"" _valid_id(const char16_t* str, std::size_t s)
+{
+  return valid_id(cx::make_string_view(str, s));
+}
+
+constexpr cx::u32string_view operator"" _valid_id(const char32_t* str, std::size_t s)
+{
+  return valid_id(cx::make_string_view(str, s));
+}
+
+constexpr cx::wstring_view operator"" _valid_id(const wchar_t* str, std::size_t s)
+{
+  return valid_id(cx::make_string_view(str, s));
+}
+
+int main()
+{
+  // compile time
+  {
+    constexpr auto id1 = valid_id("test_something");
+    std::cout << "Valid ID: " << id1 << std::endl;
+
+    constexpr auto id2 = "test_something_else"_valid_id;
+    std::cout << "Valid ID: " << id2 << std::endl;
+
+    // constexpr auto id3 = "tst_something_different"_valid_id;  // compile time error
+    // std::cout << "Valid ID: " << id3 << std::endl;
   }
 
-  if (argc != 3)
+  // runtime
+  try
   {
-    std::cerr << "invalid arguments" << std::endl;
-    return EXIT_FAILURE;
+    auto id1 = valid_id("test_something");
+    std::cout << "Valid ID: " << id1 << std::endl;
+
+    auto id2 = "test_something_else"_valid_id;
+    std::cout << "Valid ID: " << id2 << std::endl;
+
+    auto id3 = "tst_something_different"_valid_id;  // runtime error
+    std::cout << "Valid ID: " << id3 << std::endl;
   }
-
-  auto string = cx::make_string_view(argv[1], std::strlen(argv[1]));
-  auto pattern = cx::make_string_view(argv[2], std::strlen(argv[2]));
-
-  if (!wildcards::match(string, pattern))
+  catch (const std::exception& e)
   {
-    return EXIT_FAILURE;
+    std::cerr << e.what() << std::endl;
   }
 
   return EXIT_SUCCESS;
