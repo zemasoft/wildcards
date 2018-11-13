7d9a71dff38b3c9a148bc71bd0d437e4139524d7
Tomas Zeman <zemasoft@seznam.cz>
2018-11-13 13:16:04 +0100

    Fix builds

--- include/config.hpp
+++ include/config.hpp
@@ -16,7 +16,7 @@
 #define cfg_constexpr14
 #endif
 
-#if cfg_HAS_CONSTEXPR14 // && !(defined(__GNUC__) && !defined(__clang__))
+#if cfg_HAS_CONSTEXPR14 && defined(__clang__)
 #define cfg_HAS_FULL_FEATURED_CONSTEXPR14 1
 #else
 #define cfg_HAS_FULL_FEATURED_CONSTEXPR14 0
